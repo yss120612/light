48ec6094de0bdd553e9d985642b678192d37f85e
yss120612 <yss1@yandex.ru>
2022-10-18 08:08:46 +0800

    123

--- src/RELTask.cpp
+++ src/RELTask.cpp
@@ -35,10 +35,11 @@ void RELTask::setup()
 void RELTask::loop()
 {
   uint32_t command;
-  uint8_t comm=command>>16 & 0x0000FFFF;
-  uint8_t act=command & 0x0000FFFF;
+  
   if (xTaskNotifyWait(0, 0, &command, portMAX_DELAY))
   {
+    uint8_t comm=command>>16 & 0x0000FFFF;
+    uint8_t act=command & 0x0000FFFF;
     switch (comm)
     {
     case 1:
